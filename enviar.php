47daf43b11d13b5b4d7de8542bd2c05134acdfc4
BernardoFV28 <bernardo.8350855@aluno.mg.gov.br>
2025-05-01 11:52:57 -0300

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
+
+    $to = "seuemail@dominio.com";
+    $subject = "Mensagem de $nome";
+    $body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email enviado!";
+    } else {
+        echo "Erro ao enviar email.";
+    }
+}
+?>
